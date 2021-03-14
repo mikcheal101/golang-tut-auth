2c3723eaebf126779edfc7f95bb73c255bb683eb
mikcheal101 <hirekaanmicheal@gmail.com>
2021-03-14 01:59:46 +0100

    hemen/seeders

--- db.sql
+++ db.sql
@@ -2,4 +2,10 @@ create table users (
     id serial primary key,
     username varchar(20) not null unique,
     password text not null
-)
+);
+
+-- seeders
+insert into users (username, password) 
+    values ('test@example.com', '1234567890');
+insert into users (username, password) 
+    values ('test123@example.com', '1234567890');
