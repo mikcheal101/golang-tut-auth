943f2e9ec05dfc1383447d15ea218d96310d9284
mikcheal101 <hirekaanmicheal@gmail.com>
2021-03-14 08:06:59 +0100

    hemen/ - go get golang.org/x/crypto/bcrypt

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
 )
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gorilla/mux"
 	"github.com/lib/pq"
@@ -14,7 +16,7 @@ import (
 var db *sql.DB
 
 type User struct {
-	ID       string `json:"id"`
+	ID       int    `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -63,10 +65,32 @@ func loginEndpoint(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("[*] login invoked!")
 }
 
+func respondWithError(w http.ResponseWriter, status int, error Error) {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(error)
+}
+
 func registerEndpoint(w http.ResponseWriter, req *http.Request) {
 	var user User
+	var error Error
 
 	json.NewDecoder(req.Body).Decode(&user)
+
+	if strings.Trim(user.Username, " ") == "" {
+		error.Message = "Username is required!"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	if strings.Trim(user.Password, " ") == "" {
+		error.Message = "Password is required!"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	handleError(err)
+	fmt.Println(hash)
 }
 
 func profileEndpoint(w http.ResponseWriter, req *http.Request) {
